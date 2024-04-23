6da562ce33138f1f37cffec1dcf28e5ca802d4cf
riley <riley@LAPTOP-QBV9V21S>
2024-04-23 15:30:11 -0700

    Add project files.

---
+++ Bank.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hill_OOPBank
+{
+    internal class Bank
+    {
+
+        private decimal _balance;
+
+        public decimal Balance
+        {
+            get { return _balance; } 
+            set { _balance = value; }
+        }
+
+        public Bank(decimal Balance)
+        {
+            _balance = Balance;
+        }
+
+        public void Checkbalance()
+        {
+            Console.Clear(); 
+            Console.WriteLine("Your Current balance is: " + _balance.ToString("C", CultureInfo.CurrentCulture));
+        }
+
+        public decimal Withdraw(decimal Balance, decimal amount)
+        {
+            Console.Clear();
+            if (_balance < amount && amount <= 500)
+            {
+                _balance = 0;
+            }
+            else if (amount > 500)
+            {
+                Console.WriteLine("Only 500 maximum may be withdrawn.\nPress any key to contiue");
+                Console.ReadLine();
+            }
+            else { 
+                _balance -= amount;
+            }
+            return _balance;
+        }
+
+        public decimal Deposit(decimal Balance, decimal amount)
+        {
+            Console.Clear();
+            return _balance += amount;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hill_OOPBank
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            decimal amount;
+            bool CheckNumber = false;
+            string numberInput = "";
+            char userKey;
+            Bank bank = new Bank(1000m);  
+            bool isRunning = true;
+            
+            while (isRunning)
+            {
+
+            Console.WriteLine("1. Check balance");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Deposit");
+            Console.WriteLine("4. Quit");
+            userKey = Console.ReadKey().KeyChar;
+
+                switch (userKey)
+                {
+                    case '1':
+                        Notice();
+                        break;
+                    case '2':
+                        Console.WriteLine("Amount to withdraw:\n");
+                        numberInput = Console.ReadLine();
+                        CheckNumber = decimal.TryParse(numberInput, out amount);
+                        if (CheckNumber)
+                        {
+                        bank.Withdraw(bank.Balance, amount);
+                        Notice();
+                        }
+                        else
+                        {
+                        Console.WriteLine("Incorrect Input");
+                        }
+                        break;
+                    case '3':
+                        Console.WriteLine("Amount to deposit:\n");
+                        numberInput = Console.ReadLine();
+                        CheckNumber = decimal.TryParse(numberInput, out amount);
+                        bank.Deposit(bank.Balance, amount);
+                        Notice();
+                        break;
+                    case '4': isRunning = false; break;
+                    default: Console.Clear(); Console.WriteLine("Invalid Input"); break;
+                }
+            }
+            void Notice()
+            {
+                bank.Checkbalance();
+                Console.WriteLine("Press any key to continue");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
